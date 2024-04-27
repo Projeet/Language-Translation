469ba0e4a78c989550dc0e702e2418d7ad50fbcf
projeet <projeet25july@gmail.com>
2024-04-27 19:34:07 +0530

    Made changes to app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
-st.write("Hello world")
+st.title("Language Translation App")
+
+language_first=st.selectbox('Choose your language',
+    ('Hindi', 'English', 'Freanch'))
+
+your_text=st.text_area("Text to translate")
