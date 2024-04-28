91b82a75eba308b8d47704f9de398abdaa24729c
projeet <projeet25july@gmail.com>
2024-04-28 10:48:03 +0530

    Wrote the streamlit code

--- LLM/language_model.py
+++ LLM/language_model.py
@@ -14,7 +14,7 @@ def get_llm(model):
     llm=ChatNVIDIA(model=model)
     return llm
 
-def get_llm_response(human_mesage,model="ai-llama3-8b"):
+def get_llm_response(human_mesage,model="ai-llama3-70b"):
     messages=[
         SystemMessage(content="You are language translater"),
         HumanMessage(content=human_mesage)
@@ -22,22 +22,22 @@ def get_llm_response(human_mesage,model="ai-llama3-8b"):
     llm=get_llm(model)
     return llm.invoke(messages).content
 
-output_language="Punjabi"
-text="What is your name brother ?"
+output_language="Hindi"
+text="Who the hell are you ?"
 human_mesage=f"""
         Given the input sentence and output language below:
         Input Sentence : {text}
         Output Language : {output_language}
         
         Please translate the input sentence into {output_language} without giving any explanation.
-        Note: Return only the translated sentence and nothing else like any Note:.
-
+        
+        Also write a story in the tranlated language.
         Translation: 
     """
 
 
-result=get_llm_response(human_mesage)
-print(result)
+# result=get_llm_response(human_mesage)
+# print(result)
 
 # print(get_model_list())
 
--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 import streamlit as st
+from LLM import language_model as lm
 
 st.title("Language Translation App")
 
-language_first=st.selectbox('Choose your language',
-    ('Hindi', 'English', 'Freanch'))
-
 your_text=st.text_area("Text to translate")
+
+
+language=st.selectbox('Choose your language',
+    ('Hindi', 'French', 'Italian', 'Korean', 'Hibrew', 'Arabic', 'Japanese', 'Russian'))
+
+output_language=language
+text=your_text
+human_mesage=f"""
+        Given the input sentence and output language below:
+        Input Sentence : {text}
+        Output Language : {output_language}
+        
+        Please translate the input sentence into {output_language} without giving any explanation.
+
+        Translation: 
+    """
+if st.button("Translate"):
+    response=lm.get_llm_response(human_mesage)
+    st.write(f"Translated Text: {response}")
+    
